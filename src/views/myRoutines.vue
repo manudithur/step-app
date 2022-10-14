c6c58aa9d9f14e160d5d485b43fe9b8a53b56875
tgaybare <tomigayba02yt@gmail.com>
2022-10-14 18:58:25 -0300

    tweaks y myRoutines

--- src/views/myRoutines.vue
+++ src/views/myRoutines.vue
@@ -4,35 +4,34 @@
     <v-main class="content">
       <v-row>
         <v-col cols="1">
-          <router-link class="RLink" to="/home"> <!--//TODO:fijarse que este bien ruteado-->
+         <router-link class="RLink" to="/home"> <!-- //TODO:fijarse que este bien ruteado-->
             <v-btn><v-icon>mdi-back</v-icon></v-btn>
           </router-link>
 
         </v-col>
         <v-col>
-          <h1 class="mt-10">My Exercises</h1>
+          <h1 class="mt-10">My Routines</h1>
         </v-col>
       </v-row>
       <v-row>
         <v-col cols="1"/>
         <v-col cols="10">
           <v-card class="rounded-xl">
-            <v-row class="align-center ma-3" v-for="(exercise, index) in exercises" :key=exercise.name>
+            <v-row class="align-center ma-3" v-for="(routine, index) in routines" :key=routine.name>
               <v-col cols="1"/>
               <v-col cols="4">
                 <li>
-                  <h4 v-if="editMode !== index">{{exercises[index].name}}</h4>
-                  <v-text-field  v-else-if="editMode === index" v-model="exercises[index].name">{{exercises[index].name}}</v-text-field>
+                  <h4>{{routines[index].name}}</h4>
                 </li>
 
               </v-col>
               <v-col cols="2"/>
               <v-col cols="1">
-                <v-btn @click="editExercise(index)"><v-icon>mdi-pencil</v-icon></v-btn>
+                <v-btn><v-icon>mdi-pencil</v-icon></v-btn> <!-- TOOO: Hacer que el router redirija a create workout con el index el url   -->
               </v-col>
               <v-col cols="1"/>
               <v-col cols="1">
-                <v-btn @click="deleteExercise(index)"><v-icon>mdi-delete</v-icon></v-btn>
+                <v-btn @click="deleteRoutine(index)"><v-icon>mdi-delete</v-icon></v-btn>
               </v-col>
             </v-row>
           </v-card>
@@ -54,31 +53,22 @@ import FooterBar from "@/components/FooterBar";
 export default {
   data() {
     return {
-      exercises: [
+      routines: [
         {
           name: "Chest Pump",
-          repsOrTime:5
         }
-      ], //fetch exercises TODO: que haga fetch de la api
-      editMode: -1,
+      ] //fetch exercises TODO: que haga fetch de la api y acomodar los capos que muestra
     }
   },
 
   methods :{
-    deleteExercise(index){
-      this.exercises.splice(index,1); //TODO: que esto no lo cambie solo en el array sino que lo mande a la api
-    },
-    editExercise(index){
-      if (this.editMode === -1 || index !== this.editMode)
-        this.editMode=index;
-      else if (index === this.editMode){
-        this.editMode = -1;
-      }
+    deleteRoutine(index){
+      this.routines.splice(index,1); //TODO: que esto no lo cambie solo en el array sino que lo mande a la api
     }
   },
 
 
-  name: "myExercises",
+  name: "myRoutines",
   components: {FooterBar,NavBar}
 }
 </script>
